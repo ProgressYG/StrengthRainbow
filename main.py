afa211d4bbb91518081692597a74186d96e4be75
ProgressYG <127670581+ProgressYG@users.noreply.github.com>
2023-10-04 16:04:46 +0900

    main.py

--- main.py
+++ main.py
@@ -23,14 +23,16 @@ chat_model = ChatOpenAI() #Chat mode
 
 st.image('banner.jpg', use_column_width=True)
 st.title('AI Supporter - StrengthRainbow')
+st.subheader('항상 좋은 기운이 너의 주변에 가득하길 기원하며..'+'\n'+ 'from SP Division No.7 member StrengthRainbow', divider='rainbow')
 
+title_name = st.text_input('너의 이름은?')  
 title = st.text_input('StrengthRainbow가 온 세상의 좋은 기운을 모두 담아 너에게 전달해야 하는 상황을 말해줘!!')  
 
 #st.button("Reset", type="primary")
-if st.button('StrengthRainbow의 좋은 기운 받아 가기'):
+if st.button('StrengthRainbow의 좋은 기운 받기'):
    with st.spinner('Wait for it...') :
-      result = chat_model.predict(title + "에 대해 세상의 모든 좋은 기운을 가진 StrengthRainbow가 자신의 기운을 온전히 전달하는 응원글을 작성해줘")
-      st.write('StrengthRainboAI Supporter - StrengthRainboww가 말하길 ~~!!', result)
+      result = chat_model.predict("너는 지금부터 세상의 모든 좋은 기운을 가진 StrengthRainbow 입니다." + title_name + "님의 '"+  title + "' 라는 상황에 대해 StrengthRainbow 로서 좋은 기운을 전달하는 응원글을 작성해줘")
+      st.write('StrengthRainboAI Supporter - StrengthRainbow가 ' + title_name + '님 에게 말하길 ~~!!\n', result)
 
 
 
